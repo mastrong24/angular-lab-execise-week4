850459c1eba210d780a687ea3fb5a83b7a3c8cc0
mastrong24 <117696864+mastrong24@users.noreply.github.com>
2023-04-13 10:17:03 +0800

    1 file modified, 1 added

---
+++ src/app/books.ts
@@ -0,0 +1,39 @@
+export interface Book {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Angular',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+  },
+  {
+    id: 2,
+    name: 'React',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+  },
+  {
+    id: 3,
+    name: 'SpringBoot',
+    price: 299,
+    description: 'SpringBoot Cookbook',
+  },
+  {
+    id: 4,
+    name: 'Microservices',
+    price: 199,
+    description: 'Microservices Cookbook',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
