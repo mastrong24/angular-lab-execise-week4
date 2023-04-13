96be7f34914ed8872a605e85628fcc359f687bdb
mastrong24 <117696864+mastrong24@users.noreply.github.com>
2023-04-13 11:00:38 +0800

    1 file modified

--- src/app/book-detail/book-detail.component.ts
+++ src/app/book-detail/book-detail.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute } from "@angular/router";
+
+import {Book,books} from "../books";
 
 @Component({
   selector: 'app-book-detail',
   templateUrl: './book-detail.component.html',
   styleUrls: ['./book-detail.component.css']
 })
-export class BookDetailComponent {
+export class BookDetailComponent implements OnInit{
+
+book : Book | undefined
+
+constructor(private activatedRoute: ActivatedRoute){
+
+}
+
+// lifecycle hook
+ngOnInit() {
+  // First get the product id from the current route.
+  const routeParams = this.activatedRoute.snapshot.paramMap;
+  const productIdFromRoute = Number(routeParams.get('book-id'));
+
+  // Find the product that correspond with the id provided in route.
+  this.book = books.find(book=> book.id === productIdFromRoute);
+}
 
 }
