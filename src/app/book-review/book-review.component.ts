e5cc2df7194ddac53322d49fe1ed3fd0394cafe7
mastrong24 <117696864+mastrong24@users.noreply.github.com>
2023-04-13 11:12:31 +0800

    2 files modified, 2 added

--- src/app/book-review/book-review.component.ts
+++ src/app/book-review/book-review.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-book-review',
   templateUrl: './book-review.component.html',
   styleUrls: ['./book-review.component.css']
 })
-export class BookReviewComponent {
+export class BookReviewComponent implements OnInit{
+
+  review!: Observable<{ name: string, review: string }[]>;
+
+  ngOnInit(): void {
+    this.review =  this.cartService.getShippingPrices();
+  }
+
+constructor(private cartService: CartService) { }
 
 }
---
+++ src/app/book.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Book } from './books';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+
+  items: Book[] = [];
+  /* . . . */
+  
+  constructor(private http : HttpClient){}
+  
+  getShippingPrices() {
+    return this.http.get<{name: string, review: string}[]>('/assets/review.json');
+  }
+  
+  // addToCart(product: Product) {
+  //   this.items.push(product);
+  // }
+  
+  getItems() {
+    return this.items;
+  }
+  
+  // clearCart() {
+  //   this.items = [];
+  //   return this.items;
+  // }
+
+
+}
