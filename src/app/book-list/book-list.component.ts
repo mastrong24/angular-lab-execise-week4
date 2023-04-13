0051207bc278a1000d023894688ee3ac64afa747
mastrong24 <117696864+mastrong24@users.noreply.github.com>
2023-04-13 10:51:23 +0800

    1 file modified

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {books} from "../books";
 
 @Component({
   selector: 'app-book-list',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class BookListComponent {
 
+  books=books;
 }
